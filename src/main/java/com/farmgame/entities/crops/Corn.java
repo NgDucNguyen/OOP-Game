d80448f83d39c1baafb6bfaffa7aac37bc45e1d7
NgDucNguyen <nguyenducnguyen27122006@gmail.com>
2025-11-05 22:34:44 +0700

    bat dau

--- src/main/java/com/farmgame/entities/crops/Corn.java
+++
@@ -1,7 +0,0 @@
-package com.farmgame.entities.crops;
-
-public class Corn extends Crop {
-    public Corn() {
-        super("Corn", 5, 8, 22);
-    }
-}
--- src/main/java/com/farmgame/entities/crops/Wheat.java
+++
@@ -1,7 +0,0 @@
-package com.farmgame.entities.crops;
-
-public class Wheat extends Crop {
-    public Wheat() {
-        super("Wheat", 3, 5, 12);
-    }
-}
